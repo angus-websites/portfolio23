e5fe481d1db1cbbbd718321966208ba9f137ae3e
Angus <angus.goody@gmail.com>
2023-10-20 17:59:44 +0100

    can fetch from directus

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <template>
     <PageContainer>
-        <p>This is the home page</p>
+        <h1>{{page.title}}</h1>
+        <p>{{page.sub_title}}</p>
     </PageContainer>
 </template>
-  
+
+<script lang="ts" setup>
+const { $directus, $readItems } = useNuxtApp()
+
+interface PageData {
+  title: number;
+  sub_title: string;  
+}
+
+const { data: page } = await useAsyncData<{ data: PageData}>('getPageDetails', () => {
+  return $directus.request(
+    $readItems('global', {
+      fields: ['title', 'sub_title']
+    })
+  )
+});
+
+</script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,29 @@
 <template>
     <PageContainer>
-        <p>This is the projects index page</p>
+        <h1>My projects</h1>
+        <ul>
+            <li v-for="project in projects" :key="project.id">
+                <h2>{{project.title}}</h2>
+            </li>
+        </ul>
     </PageContainer>
 </template>
+<script lang="ts" setup>
+const { $directus, $readItems } = useNuxtApp()
+
+interface Project {
+  id: number;
+  slug: string;
+  title: string;
+  short_description?: string;
   
+}
+
+const { data: projects } = await useAsyncData<{ data: Project[] }>('getProjects', () => {
+  return $directus.request(
+    $readItems('projects', {
+      fields: ['slug', 'title', 'short_description']
+    })
+  )
+});
+</script>
