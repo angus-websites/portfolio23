c4913074c30ec62e53f07cdee71ed76d87bfc641
Angus <angus.goody@gmail.com>
2023-10-20 18:10:27 +0100

    again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  runtimeConfig: { DIRECTUS_HOST: process.env.DIRECTUS_HOST, },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
--- plugins/directus.js
+++ plugins/directus.js
@@ -1,9 +1,10 @@
 import { createDirectus } from '@directus/sdk';
 import { rest, readItem, readItems } from '@directus/sdk/rest';
 
-const directus = createDirectus('http://localhost:8055').with(rest());
+export default defineNuxtPlugin((context) => {
+	const directusURL = context.$config.DIRECTUS_HOST;
+	const directus = createDirectus(directusURL).with(rest());
 
-export default defineNuxtPlugin(() => {
 	return {
 		provide: { directus, readItem, readItems },
 	};
