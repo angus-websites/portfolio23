ff345f1c0aa9f1aa37903441698ad2fc76904892
Angus <angus.goody@gmail.com>
2023-10-20 23:57:45 +0100

    tweaks to dark mode toggle

--- components/buttons/DarkToggle.vue
+++ components/buttons/DarkToggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggleDarkMode" class="p-2 transition-colors duration-300 rounded-full bg-zinc-400 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-50">
+  <button @click="toggleDarkMode" class="p-2 rounded-full bg-zinc-400 dark:bg-zinc-800 text-zinc-800 dark:text-zinc-50">
     <SunIcon class="h-6 w-6 hidden dark:block" />
     <MoonIcon class="h-6 w-6 dark:hidden" />
   </button>
