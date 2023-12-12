29d0d86d2e9bba0c621cdc36cddf2eb139a50107
Angus <angus.goody@gmail.com>
2023-12-12 23:54:40 +0000

    serialize needs improvements

--- composables/useSerialize.ts
+++ composables/useSerialize.ts
@@ -24,11 +24,29 @@ export function useSerialize() {
         for (const element of elements) {
             if (element.children && element.children.length > 0) {
                 switch (element.type) {
+                    case 'h1':
+                        html += `<h1>${element.children.map(child => child.text).join('')}</h1>`;
+                        break;
                     case 'h2':
                         html += `<h2>${element.children.map(child => child.text).join('')}</h2>`;
                         break;
+                    case 'h3':
+                        html += `<h3>${element.children.map(child => child.text).join('')}</h3>`;
+                        break;
+                    case 'h4':
+                        html += `<h4>${element.children.map(child => child.text).join('')}</h4>`;
+                        break;
+                    case 'h5':
+                        html += `<h5>${element.children.map(child => child.text).join('')}</h5>`;
+                        break;
+                    case 'h6':
+                        html += `<h6>${element.children.map(child => child.text).join('')}</h6>`;
+                        break;
                     case 'ul':
                         html += `<ul>${element.children.map(child => jsonToHtml([child])).join('')}</ul>`;
+                        break;
+                    case 'ol':
+                        html += `<ol>${element.children.map(child => jsonToHtml([child])).join('')}</ol>`;
                         break;
                     case 'li':
                         html += `<li>${element.children.map(child => child.text).join('')}</li>`;
@@ -41,9 +59,13 @@ export function useSerialize() {
                         html += element.children.map(child => jsonToHtml([child])).join('');
                 }
             } else {
+
+                if ('code' in element && 'text' in element) {
+                    html += `<pre>${element.text}</pre>`;
+                }
                 // Handle text nodes
-                if ('text' in element) {
-                    html += element.text;
+                else if ('text' in element) {
+                    html += `<p>${element.text}</p>`;
                 }
             }
         }
