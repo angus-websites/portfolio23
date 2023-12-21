7aeb54aae9afbde19d1c4a6cee0bfb0f67a87a7a
Angus <angus.goody@gmail.com>
2023-12-21 15:18:53 +0000

    adjusted side colour

--- components/AboutShell.vue
+++ components/AboutShell.vue
@@ -1,7 +1,7 @@
 <template>
   <section
       aria-labelledby={id}
-      class="md:border-l md:border-zinc-400/30 md:pl-6 md:dark:border-evening-sea-600/40"
+      class="md:border-l md:border-zinc-400/30 md:pl-6 md:dark:border-lunar-500/20"
   >
     <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
       <h2
