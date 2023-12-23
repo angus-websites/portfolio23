b91d6ecc4c8b6a58c03457ee2ca37b7bef89ef6b
Angus <angus.goody@gmail.com>
2023-12-23 23:06:54 +0000

    ui tweaks

--- components/project/ProjectBreadcrumb.vue
+++ components/project/ProjectBreadcrumb.vue
@@ -1,9 +1,9 @@
 <template>
-  <NuxtLink to="/projects" class="inline-flex items-center font-medium text-evening-sea-800 dark:text-aro-300 hover:underline">
-    <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0l4-4m-4 4l4 4"/>
+  <NuxtLink to="/projects" class="inline-flex rounded-lg items-center font-medium text-evening-sea-800 dark:text-aro-300 px-4 py-3 hover:bg-zinc-300/70 dark:hover:bg-nosferatu-800">
+    <svg class="fill-current w-4 h-4 mr-2 text-mulberry-700 dark:text-blade-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+      <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
     </svg>
-    Projects
+    <span>Projects</span>
   </NuxtLink>
 
 </template>
