25c4028034effe9104d6d51b725f61ccc5719b6c
Angus <angus.goody@gmail.com>
2023-12-25 14:25:55 +0000

    set width

--- components/skills/SkillIcon.vue
+++ components/skills/SkillIcon.vue
@@ -3,15 +3,19 @@
   <img
       :src="getIconUrl(skill.icon.url)"
       :alt="skill.icon.alt"
+      width="50"
+      height="50"
       :class="skill.icon_dark_mode ? 'dark:hidden' : ''"
-      class="max-w-full max-h-full object-contain"
+      class="max-w-full max-h-full object-contain m-auto"
   />
   <!-- Dark icon (only if provided) -->
   <img
       v-if="skill.icon_dark_mode"
       :src="getIconUrl(skill.icon_dark_mode.url)"
+      width="50"
+      height="50"
       :alt="skill.icon_dark_mode.alt"
-      class="hidden dark:inline-block max-w-full max-h-full object-contain"
+      class="hidden dark:inline-block max-w-full max-h-full object-contain m-auto"
   />
 </template>
 <script setup lang="ts">
